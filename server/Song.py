6a517373ed1e9694764fd8f8a3e4de587ba6ccac
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-11 23:34:47 -0500

    loading page now

---
+++ server/Song.py
@@ -0,0 +1,5 @@
+class Song:
+    def __init__(title, artist, url):
+        self.title=title
+        self.url=url
+    
--- server/app.py
+++ server/app.py
@@ -1,9 +1,25 @@
 from flask import Flask, url_for
+import requests
+from lxml import html
+from urllib.parse import quote
 app=Flask(__name__)
+downloadsnllink="http://www.downloads.nl/results/mp3/1/";#add string of song to end
+
+def searchDownloadNL(songName):
+    print ("started")
+    url=downloadsnllink+str(quote(songName))
+    page=requests.get(url)
+    print ( url)
+    tree=html.fromstring(page.text)
+    song=tree.xpath("//a[@target='_blank']")
+    print(song)
+    return song
 @app.route('/search')
 def searchForSongs():
-    return ("need content")
+    print("1")
+    searchDownloadNL("hooked on a feeling")
+    return ("done")
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-downloadsnllink="http://www.downloads.nl/results/mp3/1/";#add string of song to end
+
