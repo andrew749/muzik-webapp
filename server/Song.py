26c93206430339b4959081fbfdad4376d010e668
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-12 00:19:37 -0500

    server now returns json data with songs.

--- server/Song.py
+++ server/Song.py
@@ -1,7 +1,7 @@
 import json
 class Song:
-    def __init__(title, artist, url):
+    def __init__(self,title, url):
         self.title=title
         self.url=url
-    def songToJson():
-        json.dumps({"title":self.title,"url":self.url})
+    def songToJson(self):
+        return json.dumps({"title":self.title,"url":self.url})
--- server/app.py
+++ server/app.py
@@ -3,11 +3,11 @@ import requests
 from lxml import html
 import pdb
 import json
+from Song import Song
 from urllib.parse import quote
 app=Flask(__name__)
 downloadsnllink="http://www.downloads.nl/results/mp3/1/";#add string of song to end
 #Seaches the site and returns an array of links
-#TODO need to return object of song
 def searchDownloadNL(songName):
     print ("started")
     header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
@@ -18,14 +18,14 @@ def searchDownloadNL(songName):
     songs=tree.xpath("//div/a[@onclick]/@href")
     songArray=[]
     for song in songs:
-        songArray.append("www.downloads.nl"+song)
-        print(song)
-    return song
+        s=Song(songName,"www.downloads.nl"+song)
+        songArray.append(s)
+    return s
 @app.route('/search')
 def searchForSongs():
     print("1")
     links=searchDownloadNL("hooked on a feeling")        
-    return (links)
+    return (links.songToJson())
 if __name__ == '__main__':
     app.run(debug=True)
 
