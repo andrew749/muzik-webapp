d5f39962eca99c683595ccb4626480fe50e8d710
Andrew Codispoti <andrewcod749@gmail.com>
2015-02-01 00:37:59 -0500

    Pushed stable revision with youtube code not implemented until further notice.

--- test.py
+++ test.py
@@ -5,17 +5,15 @@ import json
 from lxml import etree
 from Song import *
 from urllib.parse import quote
-import urllib
+import urllib.urlopen
 import pdb
 songName="Hooked on a feeling"
 header = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36','accept':'text/html,application/xhtml+xml,application/xml',}
 
-url="http://gdata.youtube.com/feeds/api/videos?q="+str(quote(songName))+"&max-results=10&v=2"
+url="https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBSH5Wy0l4XSTif-8StQjmtJCcqu_uHE2c&q="+str(quote(songName))
 print(url)
 parser = etree.XMLParser(remove_blank_text=True)
-print(1)
-page=urllib.request.urlretrieve(url)
-print (2)
+page=request(url)
 tree = etree.parse(page, parser)
 root = tree.getroot()
 namespaces = {'media':'http://search.yahoo.com/mrss/'}
