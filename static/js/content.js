d547e76ee1c7d840ea3ac5747efebf8662dab662
Andrew Codispoti <andrewcod749@gmail.com>
2015-02-17 11:11:43 -0500

    now plays youtube video but breaks other functionality if youtube plays.

--- static/js/content.js
+++ static/js/content.js
@@ -1,107 +1,116 @@
 function entry(title, url){
-    this.title=title;
-    this.url=url;
+  this.title=title;
+  this.url=url;
 
 }
 var audioPlayer=0;
 var songArray=[];
 var contentMain;
 $(document).ready(function(){
-    var downloadsnllink="http://www.downloads.nl/results/mp3/1/";//add string of song to end
-    contentMain=$(".outerelement");
-    var $container = $('#content');
-    $container.masonry('bindResize')
-    // initialize
-    $container.imagesLoaded( function() {
-        $container.masonry({itemSelector:'.outerelement',isFitWidth:true});
-    });
-    var msnry = $container.data('masonry');
-    $("#searchform").submit(function(e){
-        e.preventDefault();
+  contentMain=$(".outerelement");
+  var $container = $('#content');
+  $container.masonry('bindResize')
+  // initialize
+  $container.imagesLoaded( function() {
+    $container.masonry({itemSelector:'.outerelement',isFitWidth:true});
+  });
+  var msnry = $container.data('masonry');
+  $("#searchform").submit(function(e){
+    e.preventDefault();
 
-    });
-    $("#searchbutton").click(function(e){
-        e.preventDefault();
-        handleSearch(e);
+  });
+  $("#searchbutton").click(function(e){
+    e.preventDefault();
+    handleSearch(e);
 
-    });
-    $('#searchtext').keydown(function(e) {
-        if (e.keyCode == 13) {
-            e.preventDefault();
-            handleSearch(e);
-        }
-    });
+  });
+  $('#searchtext').keydown(function(e) {
+    if (e.keyCode == 13) {
+      e.preventDefault();
+      handleSearch(e);
+    }
+  });
 
 });
 function handleSearch(e){
-    $("#content").empty();
-    $("#content").height(0);
-    $(".page").next().remove();
-    var searchString=$("#searchtext").val();
-    getSongs(searchString);
+  $("#content").empty();
+  $("#content").height(0);
+  $(".page").next().remove();
+  var searchString=$("#searchtext").val();
+  getSongs(searchString);
 
 }
 function handleSearchClick(e){
-    $("#content").empty();
-    $("#content").height(0);
-    $(".page").next().remove();
-    getSongs(e);
+  $("#content").empty();
+  $("#content").height(0);
+  $(".page").next().remove();
+  getSongs(e);
 
 }
 
 function goBack(){
-    $("#content").empty();
-    $(".page").next().remove();
-    $('#content').append(contentMain).masonry();
-    $("#backbutton").addClass("deactivatedarrow")
+  $("#content").empty();
+  $(".page").next().remove();
+  $('#content').append(contentMain).masonry();
+  $("#backbutton").addClass("deactivatedarrow")
 }
 function getSongs(songName){
-    console.log(songName);
-    $.ajax({
-        url:  "/search",
-        type: 'GET',
-        data:{'songname':songName},
-        datatype:'jsonp',
-        success: function(data) {
-            var amount=30;
-            var j=0;
-            data=JSON.parse(data);
-            songArray=[];
-            $("#backbutton").removeClass("deactivatedarrow");
-            $("#backbutton").click(function(){
-                goBack();
-            });
-            for (var x in data){
-                if(j>amount)break;
-                j++;
-                var element=$("<div/>").addClass("outerelement col-md-3");
-                var innerelement=$("<div/>").addClass("element").attr('id',x);
-                var title=$("<h2/>").text(data[x].title);
-                element.append(innerelement);
-                innerelement.append(title);
-                $('#content').append(element);
-                songArray.push(data[x].url);
-            }
-            $('body').on('click', '.element', function() {
-                // do something
-                $('.activatedEntry').removeClass('activatedEntry');
-                $(this).addClass("activatedEntry");
-                playSong(songArray[this.id]);
+  console.log(songName);
+  $.ajax({
+    url:  "/search",
+    type: 'GET',
+    data:{'songname':songName},
+    datatype:'jsonp',
+    success: function(data) {
+      var amount=30;
+      var j=0;
+      data=JSON.parse(data);
+      songArray=[];
+      $("#backbutton").removeClass("deactivatedarrow");
+      $("#backbutton").click(function(){
+        goBack();
+      });
+      for (var x in data){
+        if(j>amount)break;
+        j++;
+        var element=$("<div/>").addClass("outerelement col-md-3");
+        var innerelement=$("<div/>").addClass("element").attr('id',x);
+        var title=$("<h2/>").text(data[x].title);
+        element.append(innerelement);
+        innerelement.append(title);
+        $('#content').append(element);
+        songArray.push(data[x].url);
+      }
+      $('body').on('click', '.element', function() {
+        // do something
+        $('.activatedEntry').removeClass('activatedEntry');
+        $(this).addClass("activatedEntry");
+        playSong(songArray[this.id]);
 
-            });
-            $("#content").masonry({itemSelector:'.outerelement',isFitWidth:true, isAnimated:true});
-        }
-    });
+      });
+      $("#content").masonry({itemSelector:'.outerelement',isFitWidth:true, isAnimated:true});
+    }
+  });
 }
 function addSource(path){
-    $("#ap").attr('src',path).trigger("play");
+  $("#ap").attr('src',path).trigger("play");
 }
 function playSong(link){
-    if(!audioPlayer){
-        $("#playerholder").append($("<audio />",{autoPlay:'autoplay',controls:'controls', id:"ap", class:"col-md-12"}));
-        audioPlayer=1;
+  //handle adding the audio player to the page.
+  var isYoutube=false;
+  if(!audioPlayer){
+    if(link.search("youtube")){
+      isYoutube=true;
+      $("#playerholder").append($("<iframe/>",{height:100, width:500,src:link}));
+    }else{
+      $("#playerholder").append($("<audio />",{autoPlay:'autoplay',controls:'controls', id:"ap", class:"col-md-12"}));
     }
+    audioPlayer=1;
+  }
+  if(!isYoutube){
     addSource(link);
-    console.log("doneadding");
+  }else{
+
+  }
 
 }
