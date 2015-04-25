a626e4413170595f381b723c3abf5ba69ad689a4
Andrew Codispoti <andrewcod749@gmail.com>
2015-04-25 12:31:22 -0400

    Removed selenium work and transitioned to funamental lxml parsing.

--- mp3raid.py
+++ mp3raid.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, url_for, request, render_template
 import requests
 from lxml import html
@@ -9,23 +8,26 @@ from Song import *
 from urllib.parse import quote
 import pdb
 from urllib.request import urlopen,Request
-from selenium import webdriver
 url="http://www.mp3raid.ws/download/"
 header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
 def getSongs(songName):
-    concaturl=url+songName.replace(" ","_")+".html"
+    end=songName.replace(" ","_")+".html"
+    concaturl=url+end
     page=requests.get(concaturl,headers=header)
-    driver=webdriver.Firefox()
-    driver.get(concaturl)
-    driver.find_elements(By.XPATH,"//*[@class='index1']")
+    tree=html.fromstring(page.text)
+    elements=tree.xpath("//*[@class='index1']//a[@class='dl']")
     songArray=[]
     for song in elements:
-        #pdb.set_trace()
-        print (song.xpath("strong/text()"))
-        driver.click(song)
-        #nexturl=song.xpath("a[@class='dl']")
-        #page2=requests.get(nexturl,header)
-        #importanturl=page2.xpath("#ssilka//a/@href")
-        #songArray.append(Song(page2.xpath("//")))
+        nexturl="http://www.mp3raid.ws/search/ddl/"+song.xpath("@id")[0]+"/"+end
+        print(nexturl)
+        page2=requests.get(nexturl,headers=header)
+        tree2=html.fromstring(page2.text)
+        pdb.set_trace()
+        songName=tree2.xpath("//table/tr/*[1]/td/*[2]/text()")
+        print(songName)
+        pdb.set_trace()
+        songURL=tree2.xpath("//table/tr/*[2]/td/*[2]/text()")
+        print(songURL)
+        songArray.append(Song(songName,songURL))
 
 getSongs("hooked on a feeling")
