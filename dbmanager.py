12a9ccaebe67712df0f7c5e8dbed61ab68e0cee7
Andrew Codispoti <andrewcod749@gmail.com>
2015-07-15 19:38:38 -0400

    Updated db manager to create a connection each request.

--- dbmanager.py
+++ dbmanager.py
@@ -3,27 +3,40 @@ import pdb
 import json
 import Song
 import os
-conn=pymysql.connect(host=os.environ['RDS_HOSTNAME'],port=int(os.environ['RDS_PORT']),user=os.environ['RDS_USERNAME'],passwd=os.environ['RDS_PASSWORD'],db="Muzik")
-cursor=conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS entries (id INT AUTO_INCREMENT NOT NULL, songtitle varchar(255) unique, url MEDIUMTEXT, artist varchar(255), albumArtUrl varchar(255), verified integer,PRIMARY KEY (id))')
 
+def connect():
+    conn=pymysql.connect(host=os.environ['RDS_HOSTNAME'],port=int(os.environ['RDS_PORT']),user=os.environ['RDS_USERNAME'],passwd=os.environ['RDS_PASSWORD'],db="Muzik")
+    cursor=conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS entries (id INT AUTO_INCREMENT NOT NULL, songtitle varchar(255) unique, url MEDIUMTEXT, artist varchar(255), albumArtUrl varchar(255), verified integer,PRIMARY KEY (id))')
+    return (conn,cursor)
 
 """Creates a song Entry in the main table"""
 def addSong(title,artist,albumArtUrl):
+    instance=connect()
+    conn=instance[0]
+    cursor=instance[1]
     cursor.execute("""INSERT IGNORE INTO entries (`songtitle`, `url`, `artist`, `albumArtUrl`, `verified`) VALUES(%s,'[]',%s,%s,0)""",[conn.escape_string(title),artist,conn.escape_string(albumArtUrl)])
     conn.commit()
+    closeConnection()
 
 """Adds a song result. assuming song object is created"""
 def addSongResult(name,title,url):
+    instance=connect()
+    conn=instance[0]
+    cursor=instance[1]
     cursor.execute("""SELECT * FROM entries WHERE songtitle=%s""",[conn.escape_string(name)])
     row=cursor.fetchone()
     data=json.loads(row[2])
     data.append({title:url})
     cursor.execute("""UPDATE entries SET url=%s WHERE songtitle=%s """,[json.dumps(data),conn.escape_string(name)])
     conn.commit()
+    closeConnection()
 
 """Get all the entries of a particular song"""
 def getSongEntries(name):
+    instance=connect()
+    conn=instance[0]
+    cursor=instance[1]
     cursor.execute("""SELECT * FROM entries WHERE songtitle LIKE %s""",(conn.escape_string(name),))
     song=cursor.fetchone()
     if song is None:
@@ -38,11 +51,16 @@ def getSongEntries(name):
         return s
     else:
         return None
+    closeConnection()
 
 def printTables(name):
+    instance=connect()
+    conn=instance[0]
+    cursor=instance[1]
     print("Main Table")
     for row in cursor.execute("SELECT * FROM entries"):
         print(row)
+    closeConnection()
 
 
 def closeConnection():
