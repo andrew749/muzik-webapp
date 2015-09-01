181e8082d36bcda0f46183e0730bc20b27256dd3
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-01 10:12:23 -0400

    Error handling with mp3skull.

--- plugins/mp3skull.py
+++ plugins/mp3skull.py
@@ -2,15 +2,18 @@ import imports
 header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
 def searchMP3Skull(songName):
     url="http://mp3skull.com/mp3/"+ str(songName.replace(' ','_')+".html")
-    page=requests.get(url,headers=header)
-    tree=html.fromstring(page.text)
-    songs=tree.xpath("//div[@id='song_html']//a[text()='Download']/@href")
-    names=tree.xpath("//div[@id='song_html']//div/div/b/text()")
-    i=0
-    songArray=[]
-    for x in range(len(songs)):
-        if(i>30):
-            break
-        songArray.append({names[x]:songs[x]})
-        i+=1
+    songArray = []
+    try:
+        page=requests.get(url,headers=header)
+        tree=html.fromstring(page.text)
+        songs=tree.xpath("//div[@id='song_html']//a[text()='Download']/@href")
+        names=tree.xpath("//div[@id='song_html']//div/div/b/text()")
+        i=0
+        for x in range(len(songs)):
+            if(i>30):
+                break
+            songArray.append({names[x]:songs[x]})
+            i+=1
+    except:
+        pass
     return (songArray)
