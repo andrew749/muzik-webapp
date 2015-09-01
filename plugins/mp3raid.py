39419c515aa5d9f55f075e47ecb01a1e26597706
Andrew Codispoti <andrewcod749@gmail.com>
2015-09-01 10:11:22 -0400

    Error handling to mp3raid if we get blocked.

--- plugins/mp3raid.py
+++ plugins/mp3raid.py
@@ -4,17 +4,20 @@ header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) G
 def getMP3RaidSongs(songName):
     end=songName.replace(" ","_")+".html"
     concaturl=url+end
-    page=requests.get(concaturl,headers=header)
-    tree=html.fromstring(page.text)
-    elements=tree.xpath("//*[@class='index1']//a[@class='dl']")
-    songArray=[]
-    for song in elements:
-        nexturl="http://www.mp3raid.ca/search/ddl/"+song.xpath("@id")[0]+"/"+end
-        page2=requests.get(nexturl,headers=header)
-        tree2=html.fromstring(page2.text)
-        songName=tree2.xpath("//table/tr/*[2]/text()")[0]
-        #print(songName)
-        songURL=tree2.xpath("//table/tr/*[2]/text()")[1]
-        #print(songURL)
-        songArray.append({songName:songURL})
+    songArray = []
+    try:
+        page=requests.get(concaturl,headers=header)
+        tree=html.fromstring(page.text)
+        elements=tree.xpath("//*[@class='index1']//a[@class='dl']")
+        for song in elements:
+            nexturl="http://www.mp3raid.ca/search/ddl/"+song.xpath("@id")[0]+"/"+end
+            page2=requests.get(nexturl,headers=header)
+            tree2=html.fromstring(page2.text)
+            songName=tree2.xpath("//table/tr/*[2]/text()")[0]
+            #print(songName)
+            songURL=tree2.xpath("//table/tr/*[2]/text()")[1]
+            #print(songURL)
+            songArray.append({songName:songURL})
+    except:
+        pass
     return songArray
